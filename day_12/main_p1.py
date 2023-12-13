72bc982df8e7e94cb114ef4ae2e6818a76242bf8
Stabbles1 <adam.poole93@gmail.com>
2023-12-13 17:55:19 +0000

    Still optimising day 12

--- day_12/main_p1.py
+++ day_12/main_p1.py
@@ -1,24 +1,36 @@
 import copy
 import itertools
+import json
 import re
+from functools import cache
 
 
 def read_input(filename: str) -> list[str]:
     with open(filename, "r") as f:
         return f.read().splitlines()
     
-def is_valid(candidate: str, broken:list[int]) -> bool:
+@cache
+def is_valid(combo:str, candidate: str, broken:str) -> bool:
+    candidate=json.loads(candidate)
+    combo=json.loads(combo)
+    broken=json.loads(broken)
+    for n in combo:
+        candidate[n] = "#"
+    # Reconstruct the string
+    sping_string = ""
+    for spring in candidate:
+        if spring == "?":
+            sping_string += "."
+        else: 
+            sping_string += spring
     try:
-        result = re.findall(r'(?:^|\.)*(#+)(?:\.|$)', candidate)
+        result = re.findall(r'(?:^|\.)*(#+)(?:\.|$)', sping_string)
         assert len(result) == len(broken)
         for i in range(0, len(result)):
             assert len(result[i]) == broken[i]
     except AssertionError:
-        print(f"{candidate} is invalid")
         return False
-    print(f"{candidate} VALID")
-    return True
-
+    return sping_string
 
 def num_of_valid_combinations(layout: str, broken:list[int]) -> int:
     broken_total = sum(broken)
@@ -33,26 +45,15 @@ def num_of_valid_combinations(layout: str, broken:list[int]) -> int:
     all_combinations = []
     for i in range(0,number_of_brokens_to_place):
         all_combinations.append(unknown_locations)
-    print(f"{already_broken_total=} {broken_total=}")
-    print(all_combinations)
 
     valid_combinations = set()
     for combo in itertools.product(unknown_locations, repeat=number_of_brokens_to_place):
         # Make sure there are no dupes in the combo
         if len(set(combo)) != len(combo):
             continue
-        current_layout = copy.copy(layout_list)
-        for n in combo:
-            current_layout[n] = "#"
-        # Reconstruct the string
-        sping_string = ""
-        for spring in current_layout:
-            if spring == "?":
-                sping_string += "."
-            else: 
-                sping_string += spring
-        if is_valid(sping_string, broken):
-            valid_combinations.add(sping_string)
+        combo = sorted(list(combo))
+        if is_valid(json.dumps(combo), json.dumps(layout_list), json.dumps(broken)) != False:
+            valid_combinations.add(is_valid(json.dumps(combo), json.dumps(layout_list), json.dumps(broken)))
     return len(valid_combinations)
 
 def parse_line(line) -> tuple[str, list[int]]:
@@ -60,11 +61,43 @@ def parse_line(line) -> tuple[str, list[int]]:
     broken = [int(n) for n in p2.split(",")]
     return layout, broken
 
+def simplify(layout: str, broken:list[int]):
+    # If there is a broken pipe at the start or end, we can extrapolate
+    layout_list = [c for c in layout]
+    # Pad out the left
+    for index in range(len(layout_list)):
+        if layout_list[index] == ".":
+            continue
+        if layout_list[index] == "?":
+            break # The dream is dead
+        if layout_list[index] == "#":
+            for broke in range(index+1, broken[0]+index):
+                layout_list[broke] = "#"
+            break
+    # Pad out the right
+    for index in range(-1, len(layout_list) * -1, -1):
+        if layout_list[index] == ".":
+            continue
+        if layout_list[index] == "?":
+            break # The dream is dead
+        if layout_list[index] == "#":
+            for broke in range(index-1, (broken[-1] * -1) +index, -1):
+                layout_list[broke] = "#"
+            break
+
+
+
+    simplified = ""
+    for c in layout_list:
+        simplified += c
+    print(f"{simplified=}")
+    return simplified, broken
 
 def solve(lines) -> int:
     total = 0
     for line in lines:
-        total += num_of_valid_combinations(*parse_line(line))
+        simplified, broken = simplify(*parse_line(line))
+        total += num_of_valid_combinations(simplified, broken)
     return total
     
 
--- day_12/test.py
+++ day_12/test.py
@@ -1,4 +1,4 @@
-from main_p1 import is_valid, parse_line
+from main_p1 import is_valid, parse_line, simplify
 from main_p1 import solve as p1_solve
 
 #from main_p2 import solve as p2_solve
@@ -12,23 +12,29 @@ lines = [
     "?###???????? 3,2,1",
     ]
 
-def test_is_valid():
-    solved_lines = [
-        "#.#.### 1,1,3",
-        ".#...#....###. 1,1,3",
-        ".#.###.#.###### 1,3,1,6",
-        "####.#...#... 4,1,1",
-        "#....######..#####. 1,6,5",
-        ".###.##....# 3,2,1",
-    ]
-    for line in solved_lines:
-        assert is_valid(*parse_line(line))
+# def test_is_valid():
+#     solved_lines = [
+#         "#.#.### 1,1,3",
+#         ".#...#....###. 1,1,3",
+#         ".#.###.#.###### 1,3,1,6",
+#         "####.#...#... 4,1,1",
+#         "#....######..#####. 1,6,5",
+#         ".###.##....# 3,2,1",
+#     ]
+#     for line in solved_lines:
+#         assert is_valid(*parse_line(line))
 
-    assert is_valid(*parse_line("###.### 1,1,3")) == False
-    assert is_valid(*parse_line("##..### 1,1,3")) == False
-    assert is_valid(*parse_line("....... 1,1,3")) == False
-    assert is_valid(*parse_line("####### 1,1,3")) == False
-    assert is_valid(*parse_line("#.#.###.# 1,1,3")) == False
+#     assert is_valid(*parse_line("###.### 1,1,3")) == False
+#     assert is_valid(*parse_line("##..### 1,1,3")) == False
+#     assert is_valid(*parse_line("....... 1,1,3")) == False
+#     assert is_valid(*parse_line("####### 1,1,3")) == False
+#     assert is_valid(*parse_line("#.#.###.# 1,1,3")) == False
+
+def test_simplify():
+    assert simplify(*parse_line("##???#??#?????????# 11,6")) == ("###########??######", [11,6])
+
+def test_simplify_2():
+    assert simplify(*parse_line(".#???#??#?????????#. 11,6")) == (".###########?######.", [11,6])
 
 def test_p1():
     assert p1_solve([lines[0]]) == 1
